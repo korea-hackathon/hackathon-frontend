40b8eb0b138a43fc038b5b0b78736f158fafc39a
dutexion <dutexion@gmail.com>
2023-08-25 13:37:16 +0900

    [ Route ] :: 탐색 페이지

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import QuestPage from "../pages/QuestPage";
 
 const Router = () => {
-  return <BrowserRouter>
-    <Routes>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/quest' element={<QuestPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
