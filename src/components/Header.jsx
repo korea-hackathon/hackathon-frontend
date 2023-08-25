7d12a4734115244500862a5b1ef691cb1d624137
wlalsplus100 <jiminelp@gmail.com>
2023-08-25 21:44:56 +0900

    이동현황 데스크탑 버전 헤더

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { css } from "styled-components";
+import { useLocation } from "react-router-dom";
 
-function Header(props) {
+function Header() {
+  const location = useLocation();
+  console.log(location.pathname);
+  if (location.pathname === "/condition") {
+    return (
+      <Body flex={true}>
+        <LogoStyle src="imgs/conditionLogo.png" alt="logo" />
+        <Line />
+        <NowMenu>이동현황</NowMenu>
+        <Homebutton mode={"land"} href="/Main">
+          <img src="imgs/home.png" alt="homeIcon" width={"24px"} />
+        </Homebutton>
+      </Body>
+    );
+  }
+
   return <Body></Body>;
 }
 
@@ -10,10 +26,37 @@ export default Header;
 
 const Body = styled.div`
   display: ${({ flex }) => flex && "flex"};
-  justify-content: center;
   align-items: center;
   width: 100vw;
   height: 40px;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
   background-color: white;
+`;
+
+const LogoStyle = styled.img`
+  margin: 5px;
+  width: 30px;
+`;
+
+const Homebutton = styled.a`
+  width: 24px;
+  margin: 8px;
+  margin-left: 1734px;
+  height: 24px;
+  border: 0;
+  background-color: transparent;
+  padding: 0;
+`;
+
+const NowMenu = styled.span`
+  font-family: "Noto Serif KR", serif;
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const Line = styled.div`
+  width: 1px;
+  height: 29px;
+  background-color: black;
+  margin: 5px;
 `;
--- src/pages/InnerInfo.jsx
+++ src/pages/InnerInfo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
+import MobileHeader from "../components/MobileHeader";
+import RealHeader from "../components/RealHeader";
 
 const InnerInfo = () => {
   return (
     <>
-      <Header />
+      <RealHeader />
     </>
   );
 };
