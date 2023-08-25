74ec4988d024f80763de4b3fd10fca9b85a6da75
wlalsplus100 <jiminelp@gmail.com>
2023-08-25 15:48:12 +0900

    [Styling]기본 스타일 및 컨테이너 세팅

--- src/pages/PresentCondition.jsx
+++ src/pages/PresentCondition.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import { css } from "styled-components";
+import GlobalStyle from "../style/GlobalStyle";
+
+const Container = styled.div`
+  width: 360px;
+  height: 800px;
+`;
 
 function PresentCondition() {
-  return <div>tyug</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Container>asd</Container>
+    </>
+  );
 }
 
 export default PresentCondition;
