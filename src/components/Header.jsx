7e89c4650f6510827cfb6b589fe1c77ab0a205cf
dutexion <dutexion@gmail.com>
2023-08-25 13:44:18 +0900

    [ Create ] :: 헤더

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return <Body></Body>;
+}
+
+export default Header;
+
+const Body = styled.div`
+  width: 100vw;
+  height: 40px;
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.25);
+`;
