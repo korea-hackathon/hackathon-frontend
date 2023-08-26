1be6cc8f165b3caf2952d7222a93dc813e63adb2
dutexion <dutexion@gmail.com>
2023-08-26 17:41:36 +0900

    asdgf

--- src/apis/danger.js
+++ src/apis/danger.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { baseUrl } from "./baseUrl";
 
 export const danger = async () => {
-  const response = await axios.get(`${baseUrl}/dangerous-zone`);
+  const response = await axios.get(`http://43.200.155.149:8080/dangerous-zone`);
   return response;
 };
--- src/apis/shipLocation.js
+++ src/apis/shipLocation.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import { baseUrl } from "./baseUrl";
 
 export const shipLocation = async () => {
-  const response = await axios.get(`${baseUrl}/cur/location`);
+  const response = await axios.get(`http://43.200.155.149:8080/cur/location`);
   return response;
 };
