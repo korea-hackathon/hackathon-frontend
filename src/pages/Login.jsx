6d3e9a05e3a94e91101a4ecddfbe8f0300bdeea0
dutexion <dutexion@gmail.com>
2023-08-27 06:19:21 +0900

    비

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,7 +25,7 @@ export const Login = () => {
                 <RoleInput>
                   <option>역할을 입력하세요</option>
                   <option>선장</option>
-                  <option>정비원</option>
+                  <option>정비사</option>
                   <option>운항사</option>
                   <option>기관사</option>
                   <option>항해사</option>
